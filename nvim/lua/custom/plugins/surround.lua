50b8b470df6af4ecc8c4ccb3ff80c0e99697a7f2
Kirsten Pleskot <kirstenpleskot@gmail.com>
2025-12-19 04:05:19 +0100

    nvim add surround

--- nvim/lua/custom/plugins/surround.lua
+++ nvim/lua/custom/plugins/surround.lua
@@ -1,1 +1,11 @@
-return { 'tpope/vim-surround' }
+return {
+  "kylechui/nvim-surround",
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup({
+      keymaps = {
+        visual = "<C-s>",
+      }
+    })
+  end
+}
