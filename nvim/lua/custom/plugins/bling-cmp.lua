ce04be8a4bb616f92a36f2e1e21df21d1002e4c6
Kirsten Pleskot <kirstenpleskot@gmail.com>
2025-12-19 04:03:53 +0100

    nvim: support for conjure_cmp

--- nvim/lua/custom/plugins/bling-cmp.lua
+++ nvim/lua/custom/plugins/bling-cmp.lua
@@ -1,53 +1,69 @@
 return {
-  'saghen/blink.cmp',
-  event = 'VimEnter',
-  version = '1.*',
-  dependencies = {
-
-    {
-      'L3MON4D3/LuaSnip',
-      version = '2.*',
-      build = (function()
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {},
-      opts = {},
-    },
-    'folke/lazydev.nvim',
+  {
+    'saghen/blink.compat',
+    -- use v2.* for blink.cmp v1.*
+    version = '2.*',
+    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    lazy = true,
+    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+    opts = {},
   },
+  {
+    'saghen/blink.cmp',
+    event = 'VimEnter',
+    version = '1.*',
+    dependencies = {
 
-  opts = {
-    keymap = {
-
-      preset = 'default',
-
-      ['<CR>'] = { 'accept', 'fallback' },
-    },
-
-    appearance = {
-
-      nerd_font_variant = 'mono',
-    },
-
-    completion = {
-
-      documentation = { auto_show = false, auto_show_delay_ms = 500 },
-    },
-
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'lazydev' },
-      providers = {
-        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+      {
+        'L3MON4D3/LuaSnip',
+        version = '2.*',
+        build = (function()
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {},
+        opts = {},
       },
+      'folke/lazydev.nvim',
+      -- 'PaterJason/cmp-conjure',
     },
 
-    snippets = { preset = 'luasnip' },
+    opts = {
+      keymap = {
 
-    fuzzy = { implementation = 'lua' },
+        preset = 'default',
 
-    signature = { enabled = true },
+        ['<CR>'] = { 'accept', 'fallback' },
+      },
+
+      appearance = {
+
+        nerd_font_variant = 'mono',
+      },
+
+      completion = {
+
+        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      },
+
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        providers = {
+          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          -- conjure = {
+          --   name = 'conjure',
+          --   module = 'blink.compat.source',
+          -- },
+        },
+      },
+
+      snippets = { preset = 'luasnip' },
+
+      fuzzy = { implementation = 'lua' },
+
+      signature = { enabled = true },
+    },
   },
 }
