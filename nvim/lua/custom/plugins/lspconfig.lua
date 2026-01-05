d90ad3c7a8a79201520b0f57fb4a0d5f71ab1526
Kirsten Pleskot <kirstenpleskot@gmail.com>
2026-01-05 20:28:09 +0100

    nvim: cleanup lspconfig

--- nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/lua/custom/plugins/lspconfig.lua
@@ -6,7 +6,6 @@ return {
     'mrcjkb/rustaceanvim',
     { 'mason-org/mason.nvim', opts = {} },
     'mason-org/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
 
     { 'j-hui/fidget.nvim', opts = {} },
 
@@ -14,7 +13,6 @@ return {
     'saghen/blink.cmp',
   },
   config = function()
-    require('neoconf').setup()
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
@@ -82,37 +80,16 @@ return {
         end
       end,
     })
-
-    -- vim.diagnostic.config {
-    --   severity_sort = true,
-    --   float = { border = 'rounded', source = 'if_many' },
-    --   underline = { severity = vim.diagnostic.severity.ERROR },
-    --   signs = vim.g.have_nerd_font and {
-    --     text = {
-    --       [vim.diagnostic.severity.ERROR] = '󰅚 ',
-    --       [vim.diagnostic.severity.WARN] = '󰀪 ',
-    --       [vim.diagnostic.severity.INFO] = '󰋽 ',
-    --       [vim.diagnostic.severity.HINT] = '󰌶 ',
-    --     },
-    --   } or {},
-    --   virtual_text = {
-    --     source = 'if_many',
-    --     spacing = 2,
-    --     format = function(diagnostic)
-    --       local diagnostic_message = {
-    --         [vim.diagnostic.severity.ERROR] = diagnostic.message,
-    --         [vim.diagnostic.severity.WARN] = diagnostic.message,
-    --         [vim.diagnostic.severity.INFO] = diagnostic.message,
-    --         [vim.diagnostic.severity.HINT] = diagnostic.message,
-    --       }
-    --       return diagnostic_message[diagnostic.severity]
-    --     end,
-    --   },
-    -- }
-
     local capabilities = require('blink.cmp').get_lsp_capabilities()
 
     local servers = {
+      clangd = {
+        cmd = {
+          'clangd',
+          '--background-index',
+          '--query-driver=/opt/devkitpro/devkitARM/bin/arm-none-eabi*,/usr/bin/gcc,**',
+        },
+      },
 
       lua_ls = {
 
@@ -124,30 +101,16 @@ return {
           },
         },
       },
-    }
-
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua',
-    })
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-    vim.lsp.config('racket_langserver', {
-      capabilities = capabilities,
-      filetypes = { 'rkt', 'racket' },
-    })
-    vim.lsp.enable('racket_langserver', true)
-
-    require('mason-lspconfig').setup {
-      ensure_installed = {},
-      automatic_installation = false,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
+      racket_langserver = {
+        filetypes = { 'rkt', 'racket' },
       },
     }
+    for server, config in pairs(servers) do
+      config = vim.tbl_deep_extend('force', {
+        capabilities = capabilities,
+      }, config or {})
+      vim.lsp.config(server, config)
+      vim.lsp.enable(server, true)
+    end
   end,
 }
