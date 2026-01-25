7082e5a3c2e746fdcbf68c90ca4c5f592f850f09
Kirsten Pleskot <kirstenpleskot@gmail.com>
2026-01-25 17:01:45 +0100

    nvim: cursed cmake hacks

--- nvim/lua/custom/plugins/cmake.lua
+++ nvim/lua/custom/plugins/cmake.lua
@@ -1,18 +1,28 @@
+function one_of(...)
+  local cwd = vim.fn.getcwd()
+  for k, v in pairs(arg) do
+    if string.find(cwd, v) then
+      return true
+    end
+  end
+  return false
+end
+
 return {
   'Civitasv/cmake-tools.nvim',
   config = function()
     local cmake_command = 'cmake'
     local cmake_build_directory = 'build'
     local cwd = vim.fn.getcwd()
-    if string.find(cwd, 'replicube3d') then
+    if one_of('replicube3d', 'Craftus-Next') then
       cmake_command = '/opt/devkitpro/portlibs/3ds/bin/arm-none-eabi-cmake'
       cmake_build_directory = 'build/main.debug'
     end
     require('cmake-tools').setup {
-      cmake_command = cmake_command, -- CMake command to use
-      cmake_build_directory = cmake_build_directory, -- Build directory
+      cmake_command = cmake_command,                                    -- CMake command to use
+      cmake_build_directory = cmake_build_directory,                    -- Build directory
       cmake_generate_options = { '-DCMAKE_EXPORT_COMPILE_COMMANDS=1' }, -- Additional options for CMake generation
-      cmake_soft_link_compile_commands = true, -- Auto-links the json file to root
+      cmake_soft_link_compile_commands = true,                          -- Auto-links the json file to root
 
       -- Appearance details
       cmake_runner = {
