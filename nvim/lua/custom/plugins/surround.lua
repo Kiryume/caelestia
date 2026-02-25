6c68c26794f01ed07014d30659e8c371ba6114de
Kirsten Pleskot <kirstenpleskot@gmail.com>
2026-02-25 10:15:30 +0100

    nvim: update surround keybinds

--- nvim/lua/custom/plugins/surround.lua
+++ nvim/lua/custom/plugins/surround.lua
@@ -2,10 +2,10 @@ return {
   "kylechui/nvim-surround",
   event = "VeryLazy",
   config = function()
-    require("nvim-surround").setup({
-      -- keymaps = {
-      --   visual = "<C-s>",
-      -- }
+    vim.g.nvim_surround_no_normal_mappings = true
+    vim.keymap.set("x", "<C-s>", "<Plug>(nvim-surround-visual)", {
+      desc = "Add a surrounding pair around a visual selection",
     })
+    require("nvim-surround").setup()
   end
 }
