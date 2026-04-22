f70a6de63a401ef2220b48528927c6472851b7d0
Kirsten Pleskot <kirstenpleskot@gmail.com>
2026-04-22 15:54:56 +0200

    nvim: treesitter for nvim

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -1,13 +1,21 @@
 return {
-  -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  lazy = false,
   config = function()
     require('nvim-treesitter').setup {
       auto_install = true,
 
-      highlight = { enable = true },
-      indent = { enable = true },
+      ensure_installed = {
+        'markdown',
+        'markdown_inline',
+        'cpp',
+      },
+
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
     }
   end,
 }
